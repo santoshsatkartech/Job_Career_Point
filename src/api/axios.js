16a87b6d5dc13e25c27087aa2af8ca6f5717ff6c
Santosh Satkar <252536588+santoshsatkartech@users.noreply.github.com>
2026-03-10 10:53:00 +0530

    adding railway url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'jobcareerpointbackend-production.up.railway.app',
   headers: { 'Content-Type': 'application/json' },
 });
 
